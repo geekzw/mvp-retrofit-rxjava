4201e497ac164e634435f2c8857732ba5123460c
古剑 <gujian@maihaoche.com>
2016-12-29 20:36:56 +0800

    update

--- mvp+rx+retrofit/src/main/java/com/gzw/login/data/Service.java
+++ mvp+rx+retrofit/src/main/java/com/gzw/login/data/Service.java
@@ -19,12 +19,12 @@ import rx.Observable;
 public interface Service {
 
     @POST("/users")
-    Observable<GitHubUserInfo> getUserInfo1(@Body UserRequest request);
+    Observable<GitHubUserInfo> getUserInfo1(@Body UserRequest request);//post方式通过gson的conver把request对象转换成RequestBody，通过gson把ResponseBody转换成GitHubUserInfo
 
     @GET("/users/{user}")
-    Observable<GitHubUserInfo> getUserInfo2(@Path("user") String user);
+    Observable<GitHubUserInfo> getUserInfo2(@Path("user") String user);//Path注解的意思是替换路径中的参数，例如调用getUserInfo2("gzw");/users/gzw
 
     @GET("/users")
-    Observable<GitHubUserInfo> getUserInfo3(@Query("user") String user);
+    Observable<GitHubUserInfo> getUserInfo3(@Query("user") String user);//Query注解是在路径后面添加参数，例如调用getUserInfo3("gzw");/users?user=gzw
 
 }
